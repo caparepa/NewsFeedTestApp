861f410fe4debdd6c6f66c3620f630e15b94e485
C.J.M. Serrano <serrano.cjm@gmail.com>
2021-08-17 11:27:57 -0400

    Add project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    kotlin("android")
+    id("kotlin-kapt")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -35,13 +38,48 @@ android {
 }
 
 dependencies {
-
+    //Core dependencies
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+}
+
+dependencies {
+    //Test dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+dependencies {
+    //Third-party dependencies
+
+    //Version define
+    val koin_version by extra("3.1.0")
+    val retrofit_version by extra("2.9.0")
+    val okhttp_version by extra("4.5.0")
+    val room_version by extra("2.3.0")
+
+    //OKHTTP
+    implementation("com.squareup.okhttp3:okhttp:$okhttp_version")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+
+    //Dependency injection
+    // Koin for Kotlin apps core
+    implementation("io.insert-koin:koin-core:$koin_version")
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+
+    // Koin for android
+    implementation("io.insert-koin:koin-android:$koin_version")
+
+    //room
+    implementation("androidx.room:room-ktx:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 }
